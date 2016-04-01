77ac17473591473d2ec9b73c3e0da81977ba9fdd
crewstyle <ach@takeatea.com>
2016-04-01 19:00:43 +0200

    add required files

--- lib/capistrano/olympus.rb
+++ lib/capistrano/olympus.rb
@@ -1,3 +1,6 @@
+require 'colorize'
+require 'sshkit/sudo'
+
 # Core tasks for deploying Olympus framework with WordPress
 load File.expand_path("../tasks/capistrano-deploy.rake", __FILE__)
 load File.expand_path("../tasks/database.rake", __FILE__)
